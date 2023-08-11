73b9f4ae5c98150f47fc230130e3c2cddabae9b6
skysmyoo7 <skysmyoo7@gmail.com>
2023-08-11 15:22:10 +0900

    배열 조각하기 해결

--- src/main/kotlin/IntArray.kt
+++ src/main/kotlin/IntArray.kt
@@ -1,31 +1,22 @@
 class Solution {
-    fun solution(rank: IntArray, attendance: BooleanArray): Int {
-        val rankMap = mutableMapOf<Int, Boolean>()
-        for (i in rank.indices) {
-            rankMap[rank[i]] = attendance[i]
+    fun solution(arr: IntArray, query: IntArray): IntArray {
+        var answer = arr.toList()
+        query.indices.forEach { i ->
+            if (i % 2 == 0) {
+                answer = answer.filterIndexed { index, _ -> index <= query[i] }
+            } else {
+                answer = answer.filterIndexed { index, _ -> index >= query[i] }
+            }
         }
-        val rankList = rankMap.filterValues { it }.keys.sorted()
-        return rank.indexOf(rankList[0]) * 10000 + rank.indexOf(rankList[1]) * 100 + rank.indexOf(rankList[2]) * 1
+        return answer.toIntArray()
     }
 }
 
 fun main() {
     println(
         Solution().solution(
-            intArrayOf(3, 7, 2, 5, 4, 6, 1),
-            booleanArrayOf(false, true, true, true, true, false, false)
-        )
-    )
-    println(
-        Solution().solution(
-            intArrayOf(1, 2, 3),
-            booleanArrayOf(true, true, true)
-        )
-    )
-    println(
-        Solution().solution(
-            intArrayOf(6, 1, 5, 2, 3, 4),
-            booleanArrayOf(true, false, true, false, false, true)
-        )
+            intArrayOf(0, 1, 2, 3, 4, 5),
+            intArrayOf(4, 1, 2),
+        ).toList()
     )
 }
