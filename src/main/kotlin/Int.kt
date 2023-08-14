8a3105fbcfe22fb7dc9d090e14d6f32606d85557
skysmyoo7 <skysmyoo7@gmail.com>
2023-08-14 17:10:11 +0900

    명예의 전당 문제 해결

--- src/main/kotlin/Int.kt
+++ src/main/kotlin/Int.kt
@@ -1,27 +1,29 @@
 class Solution4 {
-    private var ansList: MutableList<IntArray>? = null
-    fun solution(n: Int): Array<IntArray> {
-        ansList = mutableListOf()
-        dfs(n, 1, 3, 2)
-        val answer = ansList!!.toTypedArray()
-        for (i in ansList!!.indices) {
-            answer[i] = ansList!![i]
+    fun solution(k: Int, score: IntArray): IntArray {
+        val answer = mutableListOf<Int>()
+        val scoreList = mutableListOf<Int>()
+        score.forEach {
+            scoreList.add(it)
+            if (scoreList.size > k) {
+                scoreList.remove(scoreList.minOf { it })
+            }
+            answer.add(scoreList.minOf { it })
         }
-        return answer
-    }
-
-    private fun dfs(n: Int, start: Int, to: Int, mid: Int) {
-        if (n == 1) {
-            ansList?.add(intArrayOf(start, to))
-            return
-        }
-        dfs(n - 1, start, mid, to)
-        ansList?.add(intArrayOf(start, to))
-        dfs(n - 1, mid, to, start)
+        return answer.toIntArray()
     }
 }
 
 fun main() {
-    println(Solution4().solution(2).toList())
-    println(Solution4().solution(3).toList())
+    println(
+        Solution4().solution(
+            3,
+            intArrayOf(10, 100, 20, 150, 1, 100, 200)
+        ).toList()
+    )
+    println(
+        Solution4().solution(
+            4,
+            intArrayOf(0, 300, 40, 300, 20, 70, 150, 50, 500, 1000)
+        ).toList()
+    )
 }
