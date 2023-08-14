e0c03f79ba8016163d99ffe41682cd247ffd67d8
skysmyoo7 <skysmyoo7@gmail.com>
2023-08-14 16:44:24 +0900

    추억 점수 메기기 해결

--- src/main/kotlin/StringArray.kt
+++ src/main/kotlin/StringArray.kt
@@ -1,38 +1,55 @@
 class Solution3 {
-    fun solution(picture: Array<String>, k: Int): Array<String> {
-        var answer = mutableListOf<String>()
-        for(s in picture) {
-            var kStr = ""
-            for(c in s) {
-                repeat(k) {
-                    kStr += c
+    fun solution(name: Array<String>, yearning: IntArray, photo: Array<Array<String>>): IntArray {
+        val answer = mutableListOf<Int>()
+        val yearningMap = mutableMapOf<String, Int>()
+        for(i in name.indices) {
+            yearningMap[name[i]] = yearning[i]
+        }
+        for(p in photo) {
+            var score = 0
+            for(photoName in p) {
+                if(yearningMap.keys.contains(photoName)) {
+                    score += yearningMap[photoName]!!
                 }
             }
-            repeat(k) {
-                answer.add(kStr)
-            }
+            answer.add(score)
         }
-        return answer.toTypedArray()
+        return answer.toIntArray()
     }
 }
 
 fun main() {
     println(
         Solution3().solution(
-            arrayOf(".xx...xx.", "x..x.x..x", "x...x...x", ".x.....x.", "..x...x..", "...x.x...", "....x...."),
-            2
+            arrayOf("may", "kein", "kain", "radi"),
+            intArrayOf(5, 10, 1, 3),
+            arrayOf(
+                arrayOf("may", "kein", "kain", "radi"),
+                arrayOf("may", "kein", "brin", "deny"),
+                arrayOf("kon", "kain", "may", "coni")
+            )
         ).toList()
     )
     println(
         Solution3().solution(
-            arrayOf("x.x", ".x.", "x.x"),
-            3
+            arrayOf("kali", "mari", "don"),
+            intArrayOf(11, 1, 55),
+            arrayOf(
+                arrayOf("kali", "mari", "don"),
+                arrayOf("pony", "tom", "teddy"),
+                arrayOf("con", "mona", "don")
+            )
         ).toList()
     )
     println(
         Solution3().solution(
-            arrayOf("x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x.", "x.x.x.x.x.x.x.x.x.x."),
-            10
+            arrayOf("may", "kein", "kain", "radi"),
+            intArrayOf(5, 10, 1, 3),
+            arrayOf(
+                arrayOf("may"),
+                arrayOf("kein", "deny", "may"),
+                arrayOf("kon", "coni")
+            )
         ).toList()
     )
 }
