b7dfebd5bb2ff0fe3a1f96a50df8656ff97ce05a
skysmyoo7 <skysmyoo7@gmail.com>
2023-08-14 17:28:59 +0900

    카드 셔플 문제 해결

--- src/main/kotlin/StringArray.kt
+++ src/main/kotlin/StringArray.kt
@@ -1,55 +1,39 @@
 class Solution3 {
-    fun solution(name: Array<String>, yearning: IntArray, photo: Array<Array<String>>): IntArray {
-        val answer = mutableListOf<Int>()
-        val yearningMap = mutableMapOf<String, Int>()
-        for(i in name.indices) {
-            yearningMap[name[i]] = yearning[i]
-        }
-        for(p in photo) {
-            var score = 0
-            for(photoName in p) {
-                if(yearningMap.keys.contains(photoName)) {
-                    score += yearningMap[photoName]!!
+    fun solution(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
+        val cards1Copy = cards1.toMutableList()
+        val cards2Copy = cards2.toMutableList()
+        goal.forEach {
+            when (it) {
+                cards1Copy.firstOrNull() -> {
+                    cards1Copy.removeFirst()
+                }
+
+                cards2Copy.firstOrNull() -> {
+                    cards2Copy.removeFirst()
+                }
+
+                else -> {
+                    return "No"
                 }
             }
-            answer.add(score)
         }
-        return answer.toIntArray()
+        return "Yes"
     }
 }
 
 fun main() {
     println(
         Solution3().solution(
-            arrayOf("may", "kein", "kain", "radi"),
-            intArrayOf(5, 10, 1, 3),
-            arrayOf(
-                arrayOf("may", "kein", "kain", "radi"),
-                arrayOf("may", "kein", "brin", "deny"),
-                arrayOf("kon", "kain", "may", "coni")
-            )
+            arrayOf("i", "drink", "water"),
+            arrayOf("want", "to"),
+            arrayOf("i", "want", "to", "drink", "water")
         ).toList()
     )
     println(
         Solution3().solution(
-            arrayOf("kali", "mari", "don"),
-            intArrayOf(11, 1, 55),
-            arrayOf(
-                arrayOf("kali", "mari", "don"),
-                arrayOf("pony", "tom", "teddy"),
-                arrayOf("con", "mona", "don")
-            )
-        ).toList()
-    )
-    println(
-        Solution3().solution(
-            arrayOf("may", "kein", "kain", "radi"),
-            intArrayOf(5, 10, 1, 3),
-            arrayOf(
-                arrayOf("may"),
-                arrayOf("kein", "deny", "may"),
-                arrayOf("kon", "coni")
-            )
+            arrayOf("i", "water", "drink"),
+            arrayOf("want", "to"),
+            arrayOf("i", "want", "to", "drink", "water")
         ).toList()
     )
 }
